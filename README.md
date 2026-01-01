2a5de99ab33981c51f1e9a7b0d727aacbe18b4a9
Navin3d <smnavin65@gmail.com>
2026-01-01 23:50:51 +0530

    Added simple graph workflow

--- README.md
+++ README.md
@@ -2,6 +2,32 @@
 
 This is an simple AI Agent solves wordle and share it in social media using MCP server's ATM Telegram.
 
+![img.png](img.png)
+
+### Mermaid Diagram:
+```
+---
+config:
+  flowchart:
+    curve: linear
+---
+graph TD;
+	__start__([<p>__start__</p>]):::first
+	GUESS(GUESS)
+	VALIDATE(VALIDATE)
+	PUBLISH(PUBLISH)
+	__end__([<p>__end__</p>]):::last
+	GUESS --> VALIDATE;
+	VALIDATE --> GUESS;
+	VALIDATE --> PUBLISH;
+	__start__ --> GUESS;
+	PUBLISH --> __end__;
+	classDef default fill:#f2f0ff,line-height:1.2
+	classDef first fill-opacity:0
+	classDef last fill:#bfb6fc
+```
+
 ## Links
 - [Wordle Solver](https://www.nytimes.com/svc/wordle/v2/2026-01-01.json)
 - [Fast MCP](https://gofastmcp.com/getting-started/quickstart)
+- [Mermaid to PIC](https://www.mermaidflow.app/editor)
--- src/agents/wordle_solver.py
+++ src/agents/wordle_solver.py
@@ -1,10 +1,12 @@
 from typing import TypedDict, Annotated
 
 from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.constants import START, END
 from langgraph.graph.message import add_messages
 from langgraph.graph.state import StateGraph
 
-from src.agents.tools import attempt_wordle_guess
+# from src.agents.tools import attempt_wordle_guess
 
 
 class WordleState(TypedDict):
@@ -20,6 +22,43 @@ _llm = ChatOllama(
     model="llama3:70b-instruct-q2_K",
     temperature=0.8,
 )
-_llm = _llm.bind_tools([attempt_wordle_guess()])
+# _llm = _llm.bind_tools([attempt_wordle_guess()])
 
-graph = StateGraph(state_schema=WordleState)
+
+def word_guess_node(state: WordleState) -> WordleState:
+    return state
+
+def guess_validate_node(state: WordleState) -> WordleState:
+    return state
+
+def result_publish_node(state: WordleState) -> WordleState:
+    return state
+
+
+NODE_NAMES = {
+    "guess": "GUESS",
+    "validate": "VALIDATE",
+    "result_publish": "PUBLISH",
+}
+
+graph_builder = StateGraph(state_schema=WordleState)
+graph_builder.add_node(NODE_NAMES["guess"], word_guess_node)
+graph_builder.add_node(NODE_NAMES["validate"], guess_validate_node)
+graph_builder.add_node(NODE_NAMES["result_publish"], result_publish_node)
+
+graph_builder.add_conditional_edges(NODE_NAMES["guess"], guess_validate_node, path_map={
+    # NODE_NAMES["guess"]:NODE_NAMES["validate"],
+    # NODE_NAMES["validate"]:NODE_NAMES["guess"],
+    # NODE_NAMES["validate"]:NODE_NAMES["result_publish"],
+})
+
+graph_builder.add_edge(START, NODE_NAMES["guess"])
+graph_builder.add_edge(NODE_NAMES["guess"], NODE_NAMES["validate"])
+graph_builder.add_edge(NODE_NAMES["validate"], NODE_NAMES["guess"])
+graph_builder.add_edge(NODE_NAMES["validate"], NODE_NAMES["result_publish"])
+graph_builder.add_edge(NODE_NAMES["result_publish"], END)
+
+memory = InMemorySaver()
+graph = graph_builder.compile(memory)
+
+print(graph.get_graph().draw_mermaid())
